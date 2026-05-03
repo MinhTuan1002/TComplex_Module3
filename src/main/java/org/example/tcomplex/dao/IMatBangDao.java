9a36f27dcfa1afcb500e99058e91e41965c228ae
MinhTuan <nmt20031002@gmail.com>
2026-05-03 22:40:46 +0700

    update

--- src/main/java/org/example/tcomplex/dao/IMatBangDao.java
+++ src/main/java/org/example/tcomplex/dao/IMatBangDao.java
@@ -1,4 +1,12 @@
 package org.example.tcomplex.dao;
 
+import org.example.tcomplex.model.MatBang;
+import java.util.List;
+
 public interface IMatBangDao {
+    List<MatBang> selectAll(String loai, String gia, String tang, String tuNgay, String denNgay);
+    void insert(MatBang matBang);
+    boolean delete(String id);
+    // Kiểm tra trùng mã (Dùng cho validation server-side)
+    boolean checkDuplicate(String id);
 }
